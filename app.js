ad8f2e96c3eb1fb37de4cf4788ab89339fc6bf37
Anuroop2111 <anuroopls2001@gmail.com>
2022-12-14 18:45:20 +0530

    third commit

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const app = Vue.createApp({
     data(){
         return {
             show_save_btn : false,
-            head_input_val : "",
+            // title : "",
             task_input_val : "",
-            task_ : []
+            // task_ : [],
+            List_ : [
+                {title:"",task_ : []}
+            ]
         }
     },
     methods: {
@@ -19,13 +22,13 @@ const app = Vue.createApp({
 
 
         addTask(){
-            this.task_.push(this.task_input_val)
+            this.List_[0].task_.push(this.task_input_val)
             this.task_input_val = ""
         },
 
         remove_(k){
             
-        this.task_.splice(k, 1);
+        this.List_[0].task_.splice(k, 1);
     
         },
 
@@ -33,7 +36,7 @@ const app = Vue.createApp({
             console.log("del")
         },
 
-        save_task_block_to_list(){
+        save_to_list(){
 
         }
     }
